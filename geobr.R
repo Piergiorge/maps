ea2d451ba0767e0eb426352ee3dbdb54d9d2e202
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2022-12-11 17:27:15 -0300

    Update geobr.R

--- geobr.R
+++ geobr.R
@@ -2,16 +2,16 @@ library(ggplot2)
 library(geobr)
 library(ggspatial)
 
-#Acessando os dados do IBGE
+#Accessing IBGE data
 rj <- read_state(code_state = "RJ",year = 2020)
 br <- read_state(code_state = "all",year = 2020)
 
-# Plotando o mapa
+# plotting
 ggplot(br) +
   aes(group = code_state) +
   geom_sf(size = 0.1,fill="white") +
   geom_sf(aes(group=code_state),data=rj,fill="red",col="#EA6E16")+
-  labs(x = "Longitude", y = "Latitude", title = "Mapa do Brasil") +
+  labs(x = "Longitude", y = "Latitude", title = "Brazil") +
   annotation_north_arrow(style = north_arrow_fancy_orienteering)+ 
   annotation_scale(location="br",height = unit(0.15,"cm"))+
   theme_bw()
